4a05b65a6fc3a91e6477fa7639598cb293807a32
Jungchaeyeon <chaeyeon1697@gmail.com>
2021-08-19 16:43:45 +0900

    thumbnail 교체

--- client/src/components/views/PlayPage/PlayPage.js
+++ client/src/components/views/PlayPage/PlayPage.js
@@ -1,39 +1,36 @@
-import React,{ useEffect, Component } from 'react'
+import React,{ Component, useEffect } from 'react'
 import { FaCode } from "react-icons/fa";
-import $ from 'jquery';
+import "../LandingPage/LandingPage.css"
 import { Link } from 'react-router-dom';
-import {buildPj} from "../../../BuildProject/Build/BuildProject.json"
-import UnityProgress from "../../../BuildProject/TemplateData/UnityProgress"
 import { ReactDOM } from 'react';
-import {render} from 'react-dom';
 
 
 class PlayPage extends Component{
-	     render(){
-		     return(
-		             <div>
-		                 <meta charSet="utf-8" />
-		                 <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-		                     <title>Unity WebGL Player | Conveyorbelt</title>
-		                     <link rel="shortcut icon" href="TemplateData/favicon.ico" />
-		                     <link rel="stylesheet" href="TemplateData/style.css" />
-		                     <script src="TemplateData/UnityProgress.js"></script>
-		                     <script src="Build/UnityLoader.js"></script>
-		                     <script>
-		                         var gameInstance = UnityLoader.instantiate("gameContainer", "Build/BuildProject.json", UnityProgress);
-		                     </script>
-		                     <div className="webgl-content">
-		                     <div id="gameContainer" style={{width: '960px', height: '600px'}} />
-		                     <div className="footer">
-		                     <div className="webgl-logo" />
-		                     <div className="fullscreen" onclick= "gameInstance.SetFullscreen(1)" />
-		                     <div className="title">Conveyorbelt</div>
-		                 </div>
-		                 </div>
-		           </div>
-	     )
-	     }
+     html =  (
+        <div>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                <title>Unity WebGL Player | Conveyorbelt</title>
+                <link rel="shortcut icon" href="TemplateData/favicon.ico" />
+                <link rel="stylesheet" href="TemplateData/style.css" />
+                <script src="TemplateData/UnityProgress.js"></script>
+                <script src="Build/UnityLoader.js"></script>
+                <script>
+                    var gameInstance = UnityLoader.instantiate("gameContainer", "Build/BuildProject.json", UnityProgress);
+                </script>
+                <div className="webgl-content">
+                <div id="gameContainer" style={{width: '960px', height: '600px'}} />
+                <div className="footer">
+                <div className="webgl-logo" />
+                <div className="fullscreen" onclick= "gameInstance.SetFullscreen(1)" />
+                <div className="title">Conveyorbelt</div>
+            </div>
+            </div>
+      </div>);
+    render(){
+        ReactDOM.render(html, document.getElementById('root'))
+    }
 }
     
-render(PlayPage, document.getElementById('root'));   
+    
 export default PlayPage
